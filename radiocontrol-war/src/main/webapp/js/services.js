7a74e74c4730a6973453c3b3cd578145dc40ee30
Jonas Birme <jonas.birme@gmail.com>
2014-07-18 00:58:28 +0200

    Added a Jasmine unit test example

--- radiocontrol-war/src/main/webapp/js/services.js
+++ radiocontrol-war/src/main/webapp/js/services.js
@@ -22,3 +22,4 @@ radiocontrolAppServices.factory('Channel', ['$resource',
         });
     }
   ]);
+
--- radiocontrol-war/src/main/webapp/test/spec/example.js
+++ radiocontrol-war/src/main/webapp/test/spec/example.js
@@ -3,3 +3,13 @@ describe("A suite", function() {
     expect(true).toBe(true);
   });
 });
+
+function add(a, b) {
+    return a + b;
+}
+
+describe("Addition", function() {
+    it("should be possible to add 2 and 6 and the sum is 8", function() {
+        expect(add(2, 6)).toBe(8);
+    });
+});
