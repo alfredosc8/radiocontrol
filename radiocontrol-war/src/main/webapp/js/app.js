748181ba843e789f0cbc2f9a19f5430ad8ac3498
Jonas Birme <jonas.birme@gmail.com>
2014-07-17 22:29:10 +0200

    Editable fields in channel list. Not saving yet

--- radiocontrol-war/src/main/webapp/js/app.js
+++ radiocontrol-war/src/main/webapp/js/app.js
@@ -3,6 +3,7 @@ var radiocontrolApp = angular.module('radiocontrolApp', [
     'ngDraggable',
     'mediaPlayer',
     'flow',
+    'xeditable',
     'radiocontrolAppControllers',
     'radiocontrolAppServices',
 ]);
@@ -42,4 +43,10 @@ $sceDelegateProvider.resourceUrlWhitelist([
      'http://aws*.mxt.se:8000/**',
      '*'
    ]);
+});
+
+radiocontrolApp.run(function(editableOptions, editableThemes) {
+    editableThemes.bs3.inputClass = 'input-sm';
+    editableThemes.bs3.buttonsClass = 'btn-sm';
+    editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 });
